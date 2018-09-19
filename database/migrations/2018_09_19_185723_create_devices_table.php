0220c85f247d981edf502f8f7b3f26601a6a577c
obaaa8@gmail.com <obaaa8@gmail.com>
2018-09-19 21:30:23 +0200

    devices table

---
+++ database/migrations/2018_09_19_185723_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('brand')->nullable();
+            $table->string('name')->nullable();
+            $table->string('serial_number')->nullable();
+            $table->string('ip')->nullable();
+            $table->string('rooms_id')->nullable();
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
